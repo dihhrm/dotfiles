0fa02453091690b4ceab2e87bc6547f7ca26fae1
Diego Rodrigues <diego_rodrigues94@outlook.com>
2025-12-29 22:27:07 +0200

    autocmd for highlight yanked text

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,16 @@
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "Podfile",
   callback = function()
     vim.bo.filetype = "ruby"
-  end
+  end,
+})
+
+-- Highlight when yanking text
+--
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
