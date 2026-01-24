cc5e30d4c8d8297591c1c82b9085927d374d7692
Diego Rodrigues <diego@Diegos-MBP.Home>
2026-01-24 13:09:52 +0000

    add lazy git tmux, my previous mapping to manage windows

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -20,11 +20,27 @@ map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
+-- Split window
+map("n", "ss", ":split<Return>", opts)
+map("n", "sv", ":vsplit<Return>", opts)
+
 -- Resize windows with Ctrl+Shift+arrows (macOS friendly)
 map("n", "<C-S-Up>", "<cmd>resize +5<CR>", opts)
 map("n", "<C-S-Down>", "<cmd>resize -5<CR>", opts)
 map("n", "<C-S-Left>", "<cmd>vertical resize -5<CR>", opts)
 map("n", "<C-S-Right>", "<cmd>vertical resize +5<CR>", opts)
+
+-- Move window
+map("n", "sh", "<C-w>h")
+map("n", "sk", "<C-w>k")
+map("n", "sj", "<C-w>j")
+map("n", "sl", "<C-w>l")
+
+-- Better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
 -- Better line start/end (more comfortable than $ and ^)
 map("n", "gl", "$", { desc = "Go to end of line" })
